4f8ee81f9e19e5b2dc4ef64670e5ba78f82d498d
TaraMarkovic <taramarkovic6@gmail.com>
2024-09-07 17:03:42 +0200

    Added all elements to the Header component

--- prodajamodnihproizvoda/src/components/Header.tsx
+++ prodajamodnihproizvoda/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
+import { FiSearch, FiLogOut } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
+import { IoMdCart } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -8,8 +11,27 @@ const Header = () => {
       <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
         {/* Search Field */}
+        <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600 group">
+          <FiSearch className="text-gray-500 group-focus-within:text-darkText duration-200" />
+          <input
+            type="text"
+            placeholder="Search for products"
+            className="placeholder:text-sm flex-1 outline-none"
+          />
+        </div>
         {/* Login/Register */}
+        <div className="headerDiv cursor-pointer">
+          <AiOutlineUser className="text-2xl" />
+          <p className="text-sm font-semibold">Login/Register</p>
+        </div>
         {/* Cart */}
+        <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-orange-600 duration-200 relative">
+          <IoMdCart className="text-xl" />
+          <p className="text-sm font-semibold">$0.00</p>
+          <span className="bg-white text-orange-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">
+            0
+          </span>
+        </div>
       </Container>
     </div>
   );
--- prodajamodnihproizvoda/src/components/Logo.tsx
+++ prodajamodnihproizvoda/src/components/Logo.tsx
@@ -7,7 +7,6 @@ const Logo = () => {
       <h3 className="text-3xl font-semibold hover:text-orange-500 cursor-pointer duration-200">
         fashion.
       </h3>
-      ;
     </Link>
   );
 };
